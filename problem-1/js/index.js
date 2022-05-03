1ebf62ea41add1bfe966bb56cf1a7e9378b54cb5
Sravan Kumar Reddy <bonthasravan@gmail.com>
2022-05-03 14:50:14 +0530

    initial commit

---
+++ problem-1/js/index.js
@@ -0,0 +1,6 @@
+function dropdownToggle1() {
+    document.getElementById("dropdown-1").classList.toggle("show");
+}
+function dropdownToggle2() {
+    document.getElementById("dropdown-2").classList.toggle("show");
+}
